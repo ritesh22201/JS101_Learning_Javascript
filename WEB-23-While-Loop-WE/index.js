7b1562064fac90a10072aff029f09a0d247bc766
ritesh22201 <riteshgoswami22201@gmail.com>
2022-10-14 22:07:43 +0530

    WEB-23 While Loop IW

---
+++ WEB-23-While-Loop-WE/index.js
@@ -0,0 +1,13 @@
+// You need to print 13 to 18.
+let min = 13;
+let max = 19;
+while(min<max){
+  min++;
+  if(min==15) {
+    continue;
+  } console.log(min);
+  
+}
+
+  
+    
---
+++ WEB-23-While-Loop-WE/problem1.js
@@ -0,0 +1,7 @@
+//Problem 1: Print the numbers from the given starting point till ending point (including both start and end)
+
+let i = 3;
+while (i<=20){
+  console.log(i);
+  i++;
+}
