c039156f8b02337545809f9e30689cd5ca4681b9
ritesh22201 <riteshgoswami22201@gmail.com>
2022-10-14 22:01:39 +0530

    WEB-23 Logical operator and Ternary operator

---
+++ WEB-23-Logical-Operator-and-Ternary-Operator-IW/problem4.js
@@ -0,0 +1,9 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+a>b && a>c ?  console.log (a,"is greatest") : 
+  b>a && b>c ? console.log (b,"is greatest") :
+  console.log (c,"is greatest");
